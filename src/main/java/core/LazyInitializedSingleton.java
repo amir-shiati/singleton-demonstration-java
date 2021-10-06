b9483e6ca7a50af3a06d6bf6295d81df290a6b44
amir shiati <amirshiati98@gmail.com>
2021-10-06 11:52:37 +0330

    added Lazy Initialized Singleton

---
+++ src/main/java/core/LazyInitializedSingleton.java
@@ -0,0 +1,15 @@
+package core;
+
+/**
+ * To implement Singleton pattern creates the instance in the global access method.
+ */
+public class LazyInitializedSingleton {
+    private static LazyInitializedSingleton instance;
+
+    private LazyInitializedSingleton() {
+    }
+
+    public static LazyInitializedSingleton getInstance() {
+        return instance == null ? instance = new LazyInitializedSingleton() : instance;
+    }
+}
