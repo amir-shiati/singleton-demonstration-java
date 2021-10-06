0ba65a0797bfff7a71cb683486b72b44fc9e3784
amir shiati <amirshiati98@gmail.com>
2021-10-06 11:49:21 +0330

    added Static Block Singleton

---
+++ src/main/java/core/StaticBlockSingleton.java
@@ -0,0 +1,25 @@
+package core;
+
+/**
+ * Same exact principle as the EagerInitializedSingleton.
+ * except hat instance of class is created in the static block that provides option for exception handling.
+ */
+public class StaticBlockSingleton {
+    private static StaticBlockSingleton instance;
+
+    private StaticBlockSingleton() {
+    }
+
+    static {
+        try {
+            instance = new StaticBlockSingleton();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public static StaticBlockSingleton getInstance() {
+        return instance;
+    }
+
+}
