f30d09e78280c30d578bdd170a5c0a8b8bbc49b4
amir shiati <amirshiati98@gmail.com>
2021-10-06 11:32:36 +0330

    added Eager Initialized Singleton

---
+++ src/main/java/core/EagerInitializedSingleton.java
@@ -0,0 +1,15 @@
+package core;
+
+/**
+ * Instance is created even though client application might not be using it.
+ */
+public class EagerInitializedSingleton {
+    private static final EagerInitializedSingleton instance = new EagerInitializedSingleton();
+
+    private EagerInitializedSingleton() {
+    }
+
+    public static EagerInitializedSingleton getInstance() {
+        return instance;
+    }
+}
